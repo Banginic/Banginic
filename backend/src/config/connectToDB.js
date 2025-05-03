00f622a578842f6014c9c992b09ace79c0124046
Binary Digitz <borisndoh@gmail.com>
2025-05-03 15:48:06 +0100

    Add database connection file

---
+++ backend/src/config/connectToDB.js
@@ -0,0 +1,18 @@
+import app from "../server.js";
+import mongoose from "mongoose";
+import { MONGODB_URI, PORT, NODE_ENV } from "../config/env.js";
+
+function connectToDB(){
+  mongoose.connect(MONGODB_URI)
+  .then(() =>{
+    console.log('Connected to database...');
+    
+    app.listen(PORT, () =>{
+        console.log(`App is running in ${NODE_ENV} mode at http://localhost:${PORT}`);
+    })
+  })
+  .catch(() =>{
+    console.error('Failed to connect to database');
+  })
+}
+export default connectToDB;
