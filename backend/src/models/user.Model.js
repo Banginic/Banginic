fda2fe457a5f41ac4986dc94cc0bec7a91b80ca4
Binary Digitz <borisndoh@gmail.com>
2025-05-03 15:49:40 +0100

    Add User model

---
+++ backend/src/models/user.Model.js
@@ -0,0 +1,45 @@
+import jwt from 'jsonwebtoken'
+import { JWT_SECRET } from '../config/env.js'
+import mongoose from "mongoose";
+
+const authSchema = new mongoose.Schema({
+    fullName:{
+        required: true,
+        type:String,
+        minLength:3,
+        maxLength:15
+    },
+    email:{
+        required: true,
+        type:String,
+        unique: true,
+        minLength:6,
+        maxLength:20
+    },
+    phone:{
+        type:String,
+        unique: true,
+        minLength:8,
+        maxLength:15
+    },
+    password:{
+        required: true,
+        type:String,
+        minLength:8,
+        maxLength:250
+    },
+    messages:[
+        {
+            type:mongoose.Schema.ObjectId,
+            ref:'Message'
+        }
+    ]
+        
+    
+}, { timestamps: true, minimize: false})
+authSchema.methods.generateToken = function(){
+    return jwt.sign({ userId: this._id}, JWT_SECRET, { expiresIn: '1d'})
+}
+ const UserModel = mongoose.model('User', authSchema)
+
+ export default UserModel
