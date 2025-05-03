a846c693b6f30bb7fa62ba4512806a7002faa13b
Binary Digitz <borisndoh@gmail.com>
2025-05-03 17:00:35 +0100

    Add Admin Navbar

---
+++ admin/src/components/Loading.jsx
@@ -0,0 +1,11 @@
+
+
+function Loading() {
+  return (
+    <div className='grid place-items-center'>
+        <p className="text-xl px-6 py-2 lg:text-3xl bg-green-100 font-medium text-green-700">Loading.....</p>
+    </div>
+  )
+}
+
+export default Loading
