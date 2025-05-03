84b7fa3906608b1549c129fdb8506bfbd9996532
Binary Digitz <borisndoh@gmail.com>
2025-05-03 15:39:50 +0100

    Add backend index.js

---
+++ backend/src/index.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+import { JWT_SECRET } from './config/env.js'
+import  authRouter  from './routes/authRout.js'
+import connectToDB from './config/connectToDB.js'
+import errorHandler from './middlewares/errorHandler.js'
+import messageRouter from './routes/messageRoute.js'
+import employeeRouter from './routes/employeeRoute.js'
+import testimonyRouter from './routes/testimonyRoute.js'
+import projectRouter from './routes/projectRoute.js'
+
+if(!JWT_SECRET){
+    console.log('FATAL, NO JWT SECRET');
+    console.log('Please provide a JWT PRIVATE KEY in the .env.( development | production).local');
+}
+process.on('uncaughtException', (ex) =>{
+    console.log('UNCAUGHT EXCEPTION DETECTED');
+    console.log(ex.message, ex);
+    process.exit(1)
+})
+
+const app = express()
+
+// Middlewares
+app.use(cors(['http://localhost:5173']))
+app.use(cookieParser())
+app.use(express.json())
+app.use(morgan('tiny'))
+app.use('/public', express.static('public'))
+app.use('/uploads', express.static('uploads'))
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded('true'))
+
+// Route
+app.use('/api/auth', authRouter)
+app.use('/api/v2/messages', messageRouter)
+app.use('/api/v2/employees', employeeRouter)
+app.use('/api/v2/projects', projectRouter)
+app.use('/api/v2/testimony', testimonyRouter)
+
+// Error handler
+app.use(errorHandler)
+
+// config
+connectToDB()
+export default app
