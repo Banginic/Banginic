b4329685e8be5cdd261b08bd65097a2d63f61f76
Binary Digitz <borisndoh@gmail.com>
2025-05-08 00:10:40 +0100

    Add News model

--- backend/src/models/news.model.js
+++ backend/src/models/news.model.js
@@ -1,3 +1,18 @@
 import mongoose from 'mongoose'
 
-const newsSchema
+const newsSchema = mongoose.Schema({
+    subject:{
+        type:String,
+        required:true,
+        maxLength:15
+    },
+    body:{
+        type:String,
+        required:true,
+        maxLength:80
+    }
+})
+
+const NewsModel = mongoose.model('New', newsSchema)
+
+export default NewsModel;
