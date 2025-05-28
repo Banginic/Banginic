058f2be73dd5d4bbe7f3647bd168b4d9c046fa1f
Binary Digitz <borisndoh@gmail.com>
2025-05-28 15:27:39 +0100

    Implement cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,63 +1,78 @@
-import express from 'express'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
-import morgan from 'morgan'
-import { JWT_SECRET } from './config/env.js'
-import  authRouter  from './routes/authRout.js'
-import connectToDB from './config/connectToDB.js'
-import messageRouter from './routes/messageRoute.js'
-import employeeRouter from './routes/employeeRoute.js'
-import testimonyRouter from './routes/testimonyRoute.js'
-import projectRouter from './routes/projectRoute.js'
-import newsRouter from './routes/newsRoute.js'
-import jobRouter from './routes/JobRoute.js'
-import newEmployeeRouter from './routes/newEmployeeRouter.js'
-import newsLetterSubscribersRouter from './routes/newsLetterSubscribersRoute.js'
-import newsletterRouter from './routes/newsLetterRoute.js'
-import errorMiddleware from './middlewares/error.middleware.js'
-import authClientRoute from './routes/authClientRoute.js'
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import { JWT_SECRET } from "./config/env.js";
+import authRouter from "./routes/authRout.js";
+import connectToDB from "./config/connectToDB.js";
+import messageRouter from "./routes/messageRoute.js";
+import employeeRouter from "./routes/employeeRoute.js";
+import testimonyRouter from "./routes/testimonyRoute.js";
+import projectRouter from "./routes/projectRoute.js";
+import newsRouter from "./routes/newsRoute.js";
+import jobRouter from "./routes/JobRoute.js";
+import newEmployeeRouter from "./routes/newEmployeeRouter.js";
+import newsLetterSubscribersRouter from "./routes/newsLetterSubscribersRoute.js";
+import newsletterRouter from "./routes/newsLetterRoute.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import authClientRoute from "./routes/authClientRoute.js";
 
-if(!JWT_SECRET){
-    console.log('FATAL, NO JWT SECRET');
-    console.log('Please provide a JWT PRIVATE KEY in the .env.( development | production).local');
+if (!JWT_SECRET) {
+  console.log("FATAL, NO JWT SECRET");
+  console.log(
+    "Please provide a JWT PRIVATE KEY in the .env.( development | production).local"
+  );
 }
-process.on('uncaughtException', (ex) =>{
-    console.log('UNCAUGHT EXCEPTION DETECTED');
-    console.log(ex.message, ex);
-    process.exit(1)
-})
+process.on("uncaughtException", (ex) => {
+  console.log("UNCAUGHT EXCEPTION DETECTED");
+  console.log(ex.message, ex);
+  process.exit(1);
+});
 
-const app = express()
+const app = express();
 
 // Middlewares
-app.use(cors(['http://localhost:5173', 'http://localhost:5174']))
-app.use(cookieParser())
-app.use(express.json())
-app.use(morgan('dev'))
-app.use('/public', express.static('public'))
-app.use('/uploads', express.static('uploads'))
+app.use(
+  cors([
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://banginic-7.onrender.com",
+    "https://banginic-admin.onrender.com",
+  ])
+);
+app.use(cookieParser());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use("/public", express.static("public"));
+app.use("/uploads", express.static("uploads"));
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.urlencoded('true'))
 
 // Route
-app.get('/', (req, res, next) => {
-  res.status(200).json({ success:200, message: "Donnot border Boss! I am fine", status: 200})
+app.get("/", (req, res, next) => {
+  res
+    .status(200)
+    .json({
+      success: 200,
+      message: "Donnot border Boss! I am fine",
+      status: 200,
+    });
 });
-app.use('/api/v2/client/me', authClientRoute)
-app.use('/api/auth', authRouter)
-app.use('/api/v2/messages', messageRouter)
-app.use('/api/v2/employees', employeeRouter)
-app.use('/api/v2/projects', projectRouter)
-app.use('/api/v2/testimonials', testimonyRouter)
-app.use('/api/v2/news', newsRouter)
-app.use('/api/v2/jobs', jobRouter)
-app.use('/api/v2/jobs/applications', newEmployeeRouter)
-app.use('/api/v2/newsletters-subscription', newsLetterSubscribersRouter)
-app.use('/api/v2/newsletters', newsletterRouter)
+app.use("/api/v2/client/me", authClientRoute);
+app.use("/api/auth", authRouter);
+app.use("/api/v2/messages", messageRouter);
+app.use("/api/v2/employees", employeeRouter);
+app.use("/api/v2/projects", projectRouter);
+app.use("/api/v2/testimonials", testimonyRouter);
+app.use("/api/v2/news", newsRouter);
+app.use("/api/v2/jobs", jobRouter);
+app.use("/api/v2/jobs/applications", newEmployeeRouter);
+app.use("/api/v2/newsletters-subscription", newsLetterSubscribersRouter);
+app.use("/api/v2/newsletters", newsletterRouter);
 
 // Error handler
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
 // config
-connectToDB()
-export default app
+connectToDB();
+export default app;
