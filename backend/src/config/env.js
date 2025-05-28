72c5b4f88caef854f8ef45cc56d0c269be61b842
Binary Digitz <borisndoh@gmail.com>
2025-05-28 14:19:08 +0100

    Switch env to production

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,5 +1,5 @@
 import { config } from "dotenv";
-config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+config({ path: `.env.${process.env.NODE_ENV || "production"}.local` });
 
 export const {
   PORT,
