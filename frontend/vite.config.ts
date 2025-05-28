761bafcbdcd72f41f84e187ac2c59e29ce7e177a
Binary Digitz <borisndoh@gmail.com>
2025-05-28 17:40:28 +0100

    Fix typos

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import tailwindscrollbar from 'tailwind-scrollbar'
+// import tailwindscrollbar from 'tailwind-scrollbar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss(), tailwindscrollbar()],
-  server:{
-    host:true,
-    port:5173
-  },
+  plugins: [react(),  tailwindcss()],
+
 })
