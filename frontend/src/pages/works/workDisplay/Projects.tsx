662fa50d8c86d31bddd9b32ab59651cc6adda496
Binary Digitz <borisndoh@gmail.com>
2025-06-11 15:32:20 +0100

    Fix render display issues

--- frontend/src/pages/works/workDisplay/Projects.tsx
+++ frontend/src/pages/works/workDisplay/Projects.tsx
@@ -35,13 +35,10 @@ function Works() {
 
   function handleActiveTabs({ isActive }: { isActive: boolean }) {
     const active = isActive
-        ? "bg-pink-400 text-white"
-        : "bg-accent/20 hover:bg-accent/40 text-white  "
-   
-    return clsx(
-      "rounded-lg trans snap-center px-6 py-3",
-      active
-    );
+      ? "bg-pink-400 text-white"
+      : "bg-accent/20 hover:bg-accent/40 text-white  ";
+
+    return clsx("rounded-lg trans snap-center px-6 py-3", active);
   }
 
   // Set seletcted category
@@ -93,15 +90,13 @@ function Works() {
   const result = projects || data?.projects;
 
   return (
-    <motion.div
-      initial={{ opacity: 0, y: 10 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.2 }}
-      transition={{ duration: 0.5, delay: 0.4 }}
+    <div
       className={`min-h-screen trans `}
     >
       <div className=" text-center lg:w-[80%] mx-auto  ">
-        <h1 className=" text-4xl md:text-5xl font-bold bg-gradient-to-r mb-2 from-accent via-pink-400 to-blue-400 bg-clip-text text-transparent montserrat text-center">OUR PROJECTS</h1>
+        <h1 className=" text-4xl md:text-5xl font-bold bg-gradient-to-r mb-2 from-accent via-pink-400 to-blue-400 bg-clip-text text-transparent montserrat text-center">
+          OUR PROJECTS
+        </h1>
         <p className="max-w-xl text-center mx-auto text-[18px] px-2">
           Each project we take on is driven by a deep understanding of our
           clients’ goals, ensuring that we create solutions that have a lasting
@@ -138,16 +133,20 @@ function Works() {
       {/* PROJECTS */}
       <div className="grid grid-cols-1 mt-12 md:grid-cols-2 xl:grid-cols-3 md:gap-4 lg:gap-12 justify-self-center-safe 2xl:gap-24">
         {result.map((project) => (
-          <article
+          <motion.article
             key={project._id}
-            className="rounded-lg shadow-accent/50 w-[95%] mx-auto trans  md:w-[350px] lg:w-[360px] 2xl:w-[400px] group md:h-[450px]  hover:shadow-lg overflow-hidden my-8"
+            initial={{ opacity: 0, y: 10 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true, amount: 0.2 }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+            className={`rounded-lg shadow-accent/50 w-[95%] mx-auto trans ${result.length < 1 ? 'hidden' : ''} md:w-[350px] lg:w-[360px] 2xl:w-[400px] group md:h-[450px]  hover:shadow-lg overflow-hidden my-8`}
           >
             <LazyLoadImage
               className="min-w-full  h-60 xl:h-52 2xl:h-60 outline-none bg-gray-20 group-hover:scale-105 trans"
               alt={placeholdeImage}
               effect="blur"
               aria-label={`Project photo`}
-               loading="lazy"
+              loading="lazy"
               wrapperProps={{
                 style: { transition: "1s" },
               }}
@@ -179,14 +178,14 @@ function Works() {
                 </svg>{" "}
               </button>
             </div>
-          </article>
+          </motion.article>
         ))}
       </div>
 
       {/* <div className={`${styles.works_outlet}`}>
         <Outlet context={params} />
       </div> */}
-    </motion.div>
+    </div>
   );
 }
 
